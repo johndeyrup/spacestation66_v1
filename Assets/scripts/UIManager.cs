93c8118df8ad4caa03a9df5a9bbad3f758a90746
John Deyrup <john.deyrup@gmail.com>
2016-04-23 19:27:38 -0400

    Added teleport and build button

---
+++ Assets/scripts/UIManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class UIManager : MonoBehaviour {
+    // Use this for initialization
+    public List<Vector3> teleportPositions;
+    void Start () {
+        teleportPositions = new List<Vector3>();
+        teleportPositions.Add(new Vector3(1.0f,0.0f,0.0f));
+	}
+
+    public void Teleport()
+    {
+        //Debug.Log(teleportPositions);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+
+    }
+}
