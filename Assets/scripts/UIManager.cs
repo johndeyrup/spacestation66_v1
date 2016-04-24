fb8cd6264faebb9fe50d0447fe8fddcbb8c77010
John Deyrup <john.deyrup@gmail.com>
2016-04-23 20:03:30 -0400

    Added build and teleport buttons

--- Assets/scripts/UIManager.cs
+++ Assets/scripts/UIManager.cs
@@ -5,10 +5,22 @@ using System.Collections.Generic;
 public class UIManager : MonoBehaviour {
     // Use this for initialization
     public List<Vector3> teleportPositions;
+    private Camera cam;
+    private bool BuildMode;
     void Start () {
+        BuildMode = false;
         teleportPositions = new List<Vector3>();
+        cam = Camera.main;
         teleportPositions.Add(new Vector3(1.0f,0.0f,0.0f));
 	}
+
+    public void Build()
+    {
+        BuildMode = true;
+        cam.transform.rotation = Quaternion.Euler(90.0f, 0.0f, 0.0f);
+        cam.transform.position = new Vector3(0.0f, 10.0f, 0.0f);
+
+    }
 
     public void Teleport()
     {
@@ -17,6 +29,10 @@ public class UIManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        if (BuildMode == true)
+        {
+            
+        }
 
 
     }
